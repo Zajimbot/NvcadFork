a1289deebf1011ab0ac692033e87d1bbb6e82644
Artur <Karturokga@gmail.com>
2025-09-24 11:25:00 +0300

    LspTsFix

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,45 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls", "tsserver", "jsserver"}
-vim.lsp.enable(servers)
+local config = require("nvchad.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local lspconfig = require("lspconfig")
+
+-- Настройка tsserver (заменяет jsserver)
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { 
+    "javascript", 
+    "javascriptreact", 
+    "javascript.jsx",
+    "typescript", 
+    "typescriptreact", 
+    "typescript.tsx" 
+  },
+  settings = {
+    typescript = {
+      suggest = {
+        completeFunctionCalls = true,
+        includeCompletionsForModuleExports = true,
+      },
+    },
+    javascript = {
+      suggest = {
+        completeFunctionCalls = true,
+        includeCompletionsForModuleExports = true,
+      },
+    },
+  },
+})
+
+-- Остальные LSP серверы
+local servers = { "html", "cssls", "gopls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
